dfd8bf5bd1aea648e30e8beca95203668ba4dbd9
sbagos27 <sbagos@csumb.edu>
2025-04-30 15:11:20 -0700

    added pizza activity

---
+++ app/src/main/java/com/example/DashDoor/PizzaActivity.java
@@ -0,0 +1,27 @@
+package com.example.DashDoor;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+
+import com.example.DashDoor.database.DashDoorRepository;
+import com.example.DashDoor.database.entities.DashDoor;
+import com.example.DashDoor.database.entities.User;
+import com.example.DashDoor.databinding.ActivityMainBinding;
+public class PizzaActivity {
+
+}
