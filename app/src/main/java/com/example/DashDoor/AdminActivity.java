37e7b3464a941e90a34f4c4304c8093ce8579f81
Natalio-m <namaturino@csumb.edu>
2025-05-05 15:41:22 -0700

    made some changes

--- app/src/main/java/com/example/DashDoor/AdminActivity.java
+++ app/src/main/java/com/example/DashDoor/AdminActivity.java
@@ -1,5 +1,9 @@
 package com.example.DashDoor;
 
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.DashDoor.database.DashDoorRepository;
@@ -7,8 +11,36 @@ import com.example.DashDoor.databinding.ActivityMainBinding;
 
 public class AdminActivity extends AppCompatActivity {
 
+
     private com.example.DashDoor.databinding.ActivityMainBinding binding;
     private DashDoorRepository repository;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = DashDoorRepository.getRepository(getApplication());
+
+        binding.introTexView.setMovementMethod(new ScrollingMovementMethod());
+        updateDisplay();
+
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();
+                inserdashdoorRecord();
+                updateDisplay();
+            }
+
+
+
+        });
+
+    }
 
 
 
