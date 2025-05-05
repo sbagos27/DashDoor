61766412bdfa57d7bc1d1ac028fd04a5bd9107b5
sbagos27 <sbagos@csumb.edu>
2025-05-05 16:21:40 -0700

    intent creation stuff

--- app/src/main/java/com/example/DashDoor/BurgerActivity.java
+++ app/src/main/java/com/example/DashDoor/BurgerActivity.java
@@ -2,8 +2,28 @@ package com.example.DashDoor;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
-public class BurgerActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.DashDoor.databinding.BurgerPlaceBinding;
+
+
+public class BurgerActivity extends AppCompatActivity {
+
+    private BurgerPlaceBinding burgerBinding;
+
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        burgerBinding = BurgerPlaceBinding.inflate(getLayoutInflater());
+        setContentView(burgerBinding.getRoot());
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Burger Place");
+        }
+    }
 
     static Intent burgerIntentFactory(Context context){
         return new Intent(context, BurgerActivity.class);
