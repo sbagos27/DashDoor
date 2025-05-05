090c23c1d3254ea179d35e09fcdedd9d3e6939d7
andrewarriors739 <andregutierrez03@gmail.com>
2025-05-05 16:26:04 -0700

    Revert "finshed activiies and added intents to the activities"
    This reverts commit b7201370b63836b1d13da59c23e53ea96e5adb1a.

--- app/src/main/java/com/example/DashDoor/OrderPageActivity.java
+++ app/src/main/java/com/example/DashDoor/OrderPageActivity.java
@@ -28,15 +28,11 @@ public class OrderPageActivity extends AppCompatActivity {
         orderPageBinding.buttonBurger.setOnClickListener(v -> {
             selectedFoodType = "Burger Place";
             showToast("Selected: Burger Place");
-            startActivity(BurgerActivity.burgerIntentFactory(getApplicationContext()));
-
         });
 
         orderPageBinding.buttonPizza.setOnClickListener(v -> {
             selectedFoodType = "Pizza";
             showToast("Selected: Pizza");
-            startActivity(PizzaActivity.pizzaIntentFactory(getApplicationContext()));
-
         });
 
         orderPageBinding.placeOrderButton.setOnClickListener(v -> {
