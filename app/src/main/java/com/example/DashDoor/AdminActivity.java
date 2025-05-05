d48d1d76a36f00b84d97512a18dab5b98178ed27
Natalio-m <namaturino@csumb.edu>
2025-05-05 05:18:55 -0700

    updated activity_admin.xml

--- app/src/main/java/com/example/DashDoor/AdminActivity.java
+++ app/src/main/java/com/example/DashDoor/AdminActivity.java
@@ -2,5 +2,15 @@ package com.example.DashDoor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.DashDoor.database.DashDoorRepository;
+import com.example.DashDoor.databinding.ActivityMainBinding;
+
 public class AdminActivity extends AppCompatActivity {
+
+    private com.example.DashDoor.databinding.ActivityMainBinding binding;
+    private DashDoorRepository repository;
+
+
+
+
 }
