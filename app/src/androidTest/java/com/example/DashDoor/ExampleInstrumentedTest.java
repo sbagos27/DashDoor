ba841bc0095b2bf3561641e572733af40e683091
sbagos27 <sbagos@csumb.edu>
2025-05-06 13:30:53 -0700

    added intent tests for going into burg and pizza

--- app/src/androidTest/java/com/example/DashDoor/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/DashDoor/ExampleInstrumentedTest.java
@@ -1,14 +1,28 @@
 package com.example.DashDoor;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
+import android.content.Intent;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.GrantPermissionRule;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,8 +33,39 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.hw04_gymlog_v300", appContext.getPackageName());
+        assertEquals("com.example.DashDoor", appContext.getPackageName());
     }
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testBurgerButtonLaunchesBurgerActivity() {
+        ActivityScenario.launch(OrderPageActivity.class);
+
+        onView(withId(R.id.buttonBurger)).perform(click());
+
+        intended(hasComponent(BurgerActivity.class.getName()));
+    }
+
+    @Test
+    public void testPizzaButtonLaunchesBurgerActivity() {
+        ActivityScenario.launch(OrderPageActivity.class);
+
+        onView(withId(R.id.buttonPizza)).perform(click());
+
+        intended(hasComponent(PizzaActivity.class.getName()));
+    }
+
+
+
+
 }
