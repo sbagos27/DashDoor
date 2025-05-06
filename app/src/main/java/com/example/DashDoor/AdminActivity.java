77694cc0381b188953a3cca790c01d4312b1fc83
Natalio-m <namaturino@csumb.edu>
2025-05-06 13:33:21 -0700

    put info in method

--- app/src/main/java/com/example/DashDoor/AdminActivity.java
+++ app/src/main/java/com/example/DashDoor/AdminActivity.java
@@ -9,18 +9,26 @@ import java.util.List;
 
 public class AdminActivity extends AppCompatActivity {
 
-    List<String> fName;
-    List<String> lName;
-    List<Integer> cNum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+
+    }
+    public static void initInfo(){
+
+        List<String> fName;
+        List<String> lName;
+        List<Integer> cNum;
+        List<Integer> fNum;
+
         fName = new ArrayList<>();
         lName = new ArrayList<>();
         cNum = new ArrayList<>();
+        fNum = new ArrayList<>();
 
 
         fName.add(0,"Lisa");
@@ -36,8 +44,26 @@ public class AdminActivity extends AppCompatActivity {
         cNum.add(0,3343);
         cNum.add(1,9102);
         cNum.add(2,7770);
-        cNum.add(3,0152);
+        cNum.add(3,3433);
+
+        fNum.add(0,831-111-4342);
+        fNum.add(1,331-215-7600);
+        fNum.add(2,111-110-2002);
+        fNum.add(3,871-980-1101);
+
+
+        for (int i=0; i < 4; i++){
+            System.out.println("First Name " + fName.get(i));
+            System.out.println("Last Name " + lName.get(i));
+            System.out.println("Card Number ************-" +cNum.get(i));
+            System.out.println("Phone Number " + fNum.get(i));
+            System.out.println("------------------------------------------------");
+
+
+        }
+
 
 
     }
+
 }
